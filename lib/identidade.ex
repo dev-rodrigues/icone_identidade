a8a5c4c7ba21b76bd067508dd4503d3850c60c54
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-03-09 14:15:09 -0300

    init

---
+++ lib/identidade.ex
@@ -0,0 +1,18 @@
+defmodule Identidade do
+  @moduledoc """
+  Documentation for Identidade.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identidade.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
