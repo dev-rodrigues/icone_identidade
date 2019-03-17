f805ca428652a86cfa67834c19c60565591df32a
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-03-16 22:09:05 -0300

    construindo pixel da imagem

--- lib/identidade.ex
+++ lib/identidade.ex
@@ -8,12 +8,13 @@ defmodule Identidade do
     |> hash_input
     |> criar_cor
     |> criar_tabela
+    |> remover_impar
+    |> constroi_pixel
   end
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
-
     %Identidade.Imagem{hex: hex}
   end
 
@@ -27,8 +28,26 @@ defmodule Identidade do
     |> Enum.map(&espelhar/1)
     |> List.flatten
     |> Enum.with_index
-
     %Identidade.Imagem{imagem | grid: grid}
+  end
+
+  def remover_impar(%Identidade.Imagem{grid: grid} = imagem)  do
+    new_grid = Enum.filter grid, fn{valor, _indice} -> 
+      rem(valor, 2) == 0
+    end
+    %Identidade.Imagem{imagem | grid: new_grid}
+  end
+
+  def constroi_pixel(%Identidade.Imagem{grid: grid} = imagem) do
+    pixel_map = Enum.map grid, fn{_valor, indice} -> 
+      h = rem(indice, 5) * 50
+      v = div(indice, 5) * 50
+      top_esquerda = {h, v}
+      inf_direita = {h + 50, v + 50}
+      {top_esquerda, inf_direita}
+    end
+
+    %Identidade.Imagem{imagem | pixel_map: pixel_map}
   end
 
   def espelhar(linha) do
